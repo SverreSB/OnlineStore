71fb3aaffe7bd2401df6bc5e256bc0f478b2aafb
Sverre Broen <sverre.sb@gmail.com>
2018-10-25 17:18:24 -0700

    initial commit

---
+++ OnlineStore/Customer.java
@@ -0,0 +1,4 @@
+package edu.csumb.cst338.homework.hw5.OnlineStore;
+
+public class Customer {
+}
---
+++ OnlineStore/Main.java
@@ -0,0 +1,8 @@
+package edu.csumb.cst338.homework.hw5.OnlineStore;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
